480c19433610eaf032933f81d603478760f0e09d
David Lynch <kemayo@gmail.com>
2024-08-13 22:46:34 -0500

    Show this session's routes on the map

--- addon.lua
+++ addon.lua
@@ -1,6 +1,8 @@
 local myname, ns = ...
 local myfullname = C_AddOns.GetAddOnMetadata(myname, "Title")
 
+ns.CLASSIC = WOW_PROJECT_ID ~= WOW_PROJECT_MAINLINE
+
 local Callbacks = CreateFrame("EventFrame")
 Callbacks:SetUndefinedEventsAllowed(true)
 Callbacks:SetScript("OnEvent", function(self, event, ...)
@@ -40,16 +42,22 @@ ns:RegisterCallback("ADDON_LOADED", function(self, event, name)
         __index = {
             threshold = 10,
             interval = 1,
+            map_raw = false,
+            map_straight = true,
             -- routes = {},
         },
     })
     db = _G[myname.."DB"]
     ns.db = db
 
+    ns.routes = {}
+
     self:UnregisterCallback("ADDON_LOADED")
     if IsLoggedIn() then self:PLAYER_LOGIN() else self:RegisterCallback("PLAYER_LOGIN") end
 end)
 function ns:PLAYER_LOGIN()
+    WorldMapFrame:AddDataProvider(ns.RouteWorldMapDataProvider)
+
     self:UnregisterCallback("PLAYER_LOGIN")
 end
 
@@ -60,9 +68,9 @@ function ns:StartRoute(threshold)
 
     local mapID = C_Map.GetBestMapForUnit("player")
     local position = C_Map.GetPlayerMapPosition(mapID, "player")
-    ns.route = {mapID = mapID, start = time()}
+    ns.route = {mapID = mapID, start = time(), raw = {},}
     local zw, zh = ns:GetZoneSize(mapID)
-    table.insert(ns.route, position)
+    table.insert(ns.route.raw, position)
     ns.ticker = C_Timer.NewTicker(db.interval, function(ticker)
         -- always on the starting mapID
         local newposition = C_Map.GetPlayerMapPosition(mapID, "player")
@@ -70,7 +78,7 @@ function ns:StartRoute(threshold)
         -- print("Moved since last:", math.sqrt(distance))
         if distanceSq > thresholdSq then
             -- TODO: detect if the previous point is on a straight line between the new point and previous-1, and remove it?
-            table.insert(ns.route, newposition)
+            table.insert(ns.route.raw, newposition)
             position = newposition
             -- print("Logged", position:GetXY())
         end
@@ -97,29 +105,28 @@ function ns:StopRoute(...)
 
     local route = self.route
     route.stop = time()
+    route.straight = {}
     ns.route = nil
 
     local zw, zh = ns:GetZoneSize(route.mapID)
     local distance = 0
-    local raw = {}
-    local straight = {}
-    for i, position in ipairs(route) do
+    for i, position in ipairs(route.raw) do
         -- Every coordinate gets logged:
-        table.insert(raw, self:GetCoord(position:GetXY()))
+        -- table.insert(raw, self:GetCoord(position:GetXY()))
         -- add the travel distance:
-        if route[i - 1] then
+        if route.raw[i - 1] then
             -- distance = distance + CalculateDistance(route[i - 1].x * zw, route[i - 1].y * zh, position.x * zw, position.y * zh)
-            distance = distance + self:CalculateDistance(route[i - 1], position, zw, zh)
+            distance = distance + self:CalculateDistance(route.raw[i - 1], position, zw, zh)
         end
         -- Now, work out whether this was superfluous:
-        if i == 1 or i == #route then
+        if i == 1 or i == #route.raw then
             -- First and last coords always get added
-            table.insert(straight, self:GetCoord(position:GetXY()))
-        elseif route[i - 1] and route[i + 1] then
+            table.insert(route.straight, position)
+        elseif route.raw[i - 1] and route.raw[i + 1] then
             -- Is this point on a straight line between the point before and after it?
             -- Check whether the distance <a to b> + <b to c> is about the same as <a to c>
-            local routedistance = self:CalculateDistance(position, route[i - 1]) + self:CalculateDistance(position, route[i + 1])
-            local straightdistance = self:CalculateDistance(route[i - 1], route[i + 1])
+            local routedistance = self:CalculateDistance(position, route.raw[i - 1]) + self:CalculateDistance(position, route.raw[i + 1])
+            local straightdistance = self:CalculateDistance(route.raw[i - 1], route.raw[i + 1])
             -- Third arg to ApproximatelyEqual is the tuning factor for the curve, and is
             -- how far the distances are allowed to deviate while still being "equal".
             -- Worst-case for this is long slow gentle curves, which will be entirely smoothed
@@ -127,13 +134,20 @@ function ns:StopRoute(...)
             -- complicated.
             -- (This is coord-scaled, so 0-1 as percent-of-zone; MathUtil.Epsilon is .000001, which is too small)
             if not ApproximatelyEqual(routedistance, straightdistance, 0.0001) then
-                table.insert(straight, self:GetCoord(position:GetXY()))
+                table.insert(route.straight, position)
             end
         end
     end
-    self:ShowTextToCopy(("%d points; %d yards traveled; %d seconds"):format(#route, distance, route.stop - route.start))
-    self:ShowTextToCopy("Raw coords", unpack(raw))
-    self:ShowTextToCopy("Straightened coords", unpack(straight))
+    local function coordify(position)
+        return ns:GetCoord(position:GetXY())
+    end
+    self:ShowTextToCopy(("%d (%d) points; %d yards traveled; %d seconds"):format(#route.straight, #route.raw, distance, route.stop - route.start))
+    self:ShowTextToCopy("Raw coords", unpack(TableUtil.Transform(route.raw, coordify)))
+    self:ShowTextToCopy("Straightened coords", unpack(TableUtil.Transform(route.straight, coordify)))
+
+    table.insert(ns.routes, route)
+
+    ns.RouteWorldMapDataProvider:RefreshAllData()
 end
 
 function ns:PositionIsWithinBounds(position)
@@ -184,6 +198,51 @@ function ns:GetXY(coord)
     return floor(coord / 10000) / 10000, (coord % 10000) / 10000
 end
 
+function ns:ShowConfigMenu()
+    local function makeRadios(key, description, ...)
+        local isSelected = function(val) return db[key] == val end
+        local setSelected = function(val)
+            db[key] = val
+            ns.RouteWorldMapDataProvider:RefreshAllData()
+            return MenuResponse.Close
+        end
+        for i=1, select("#", ...) do
+            local radio = select(i, ...) -- {text, value}
+            description:CreateRadio(radio[1], isSelected, setSelected, radio[2])
+        end
+    end
+    local checkIsSelected = function(key) return db[key] end
+    local checkSetSelected = function(key)
+        db[key] = not db[key]
+        ns.RouteWorldMapDataProvider:RefreshAllData()
+        -- return MenuResponse.Clos
+    end
+    MenuUtil.CreateContextMenu(nil, function(owner, rootDescription)
+        rootDescription:SetTag("MENU_RANGERECORDER_CONTEXT")
+        rootDescription:CreateTitle(myfullname)
+
+        local map = rootDescription:CreateButton("On map...")
+        map:CreateCheckbox("Raw points", checkIsSelected, checkSetSelected, "map_raw")
+        map:CreateCheckbox("Straightened points", checkIsSelected, checkSetSelected, "map_straight")
+
+        makeRadios("threshold",
+            rootDescription:CreateButton("Threshold"),
+            {"5 yards", 5},
+            {"10 yards", 10},
+            {"25 yards", 25},
+            {"40 yards", 40}
+        )
+        makeRadios("interval",
+            rootDescription:CreateButton("Interval"),
+            {"0.5 seconds", 0.5},
+            {"1.0 seconds", 1},
+            {"1.5 seconds", 1.5},
+            {"2.0 seconds", 2},
+            {"5.0 seconds", 5}
+        )
+    end)
+end
+
 _G.RouteRecorder_OnAddonCompartmentClick = function(addon, button, ...)
     -- DevTools_Dump({addon, button, ...})
     if button == "LeftButton" then
@@ -193,36 +252,7 @@ _G.RouteRecorder_OnAddonCompartmentClick = function(addon, button, ...)
             ns:StartRoute()
         end
     elseif button == "RightButton" then
-        local function makeRadios(key, description, ...)
-            local isSelected = function(val) return db[key] == val end
-            local setSelected = function(val)
-                db[key] = val
-                return MenuResponse.Close
-            end
-            for i=1, select("#", ...) do
-                local radio = select(i, ...) -- {text, value}
-                description:CreateRadio(radio[1], isSelected, setSelected, radio[2])
-            end
-        end
-        MenuUtil.CreateContextMenu(nil, function(owner, rootDescription)
-            rootDescription:SetTag("MENU_RANGERECORDER_CONTEXT")
-            rootDescription:CreateTitle(myfullname)
-            makeRadios("threshold",
-                rootDescription:CreateButton("Threshold"),
-                {"5 yards", 5},
-                {"10 yards", 10},
-                {"25 yards", 25},
-                {"40 yards", 40}
-            )
-            makeRadios("interval",
-                rootDescription:CreateButton("Interval"),
-                {"0.5 seconds", 0.5},
-                {"1.0 seconds", 1},
-                {"1.5 seconds", 1.5},
-                {"2.0 seconds", 2},
-                {"5.0 seconds", 5}
-            )
-        end)
+        ns:ShowConfigMenu()
     end
 end
 
---
+++ map.lua
@@ -0,0 +1,207 @@
+local myname, ns = ...
+
+-- This is copied mostly from my HandyNotes handler
+
+local RouteWorldMapDataProvider = CreateFromMixins(MapCanvasDataProviderMixin)
+ns.RouteWorldMapDataProvider = RouteWorldMapDataProvider
+
+local RoutePinMixin = CreateFromMixins(MapCanvasPinMixin)
+local RoutePinConnectionMixin = {}
+
+function RouteWorldMapDataProvider:RemoveAllData()
+    if not self:GetMap() then return end
+
+    if self.pinPool then
+        self.pinPool:ReleaseAll()
+        self.connectionPool:ReleaseAll()
+    end
+    self:GetMap().ScrollContainer:MarkCanvasDirty()
+end
+
+function RouteWorldMapDataProvider:RefreshAllData(fromOnShow)
+    if not (self:GetMap() and self:GetMap():IsShown()) then return end
+    self:RemoveAllData()
+
+    local uiMapID = self:GetMap():GetMapID()
+    if not uiMapID then return end
+
+    if not self.pinPool then
+        self:CreatePools()
+    end
+
+    for _, route in ipairs(ns.routes) do
+        if route.mapID == uiMapID then
+            self:DrawRoute(route, uiMapID)
+        end
+    end
+end
+
+function RouteWorldMapDataProvider:CreatePools()
+    self.pinPool = CreateFramePool("FRAME", self:GetMap():GetCanvas(), nil, function(pool, pin)
+        if not pin.OnReleased then
+            Mixin(pin, RoutePinMixin)
+        end
+        (_G.FramePool_HideAndClearAnchors or _G.Pool_HideAndClearAnchors)(pool, pin)
+        pin:OnReleased()
+
+        pin.pinTemplate = nil
+        pin.owningMap = nil
+    end)
+    self.connectionPool = CreateFramePool("FRAME", self:GetMap():GetCanvas(), nil, function(pool, connection)
+        if not connection.Line then
+            Mixin(connection, RoutePinConnectionMixin)
+            connection:SetIgnoreParentScale(true)
+            connection.Line = connection:CreateLine()
+        end
+        (_G.FramePool_HideAndClearAnchors or _G.Pool_HideAndClearAnchors)(pool, connection)
+    end)
+end
+
+local COLORS = {
+    raw = {r=1, g=0, b=0},
+    straight = {r=0, g=1, b=1},
+}
+function RouteWorldMapDataProvider:DrawRoute(route, uiMapID)
+    if ns.db.map_raw then
+        self:DrawPath(route.raw, uiMapID, "raw")
+    end
+    if ns.db.map_straight then
+        self:DrawPath(route.straight, uiMapID, "straight")
+    end
+end
+
+local pins = {}
+function RouteWorldMapDataProvider:DrawPath(path, uiMapID, variant)
+    for _, node in ipairs(path) do
+        local x, y = node:GetXY()
+        local pin = self:AcquirePin(variant, COLORS[variant])
+        pin:SetPosition(x, y)
+        pin:Show()
+        if pins[#pins] then
+            self:ConnectPins(pins[#pins], pin, COLORS[variant])
+        end
+        table.insert(pins, pin)
+    end
+    wipe(pins)
+end
+
+function RouteWorldMapDataProvider:ConnectPins(pin1, pin2, color)
+    local connection = self.connectionPool:Acquire()
+    connection:Connect(pin1, pin2)
+    connection.Line:SetVertexColor(color.r or 1, color.g or 1, color.b or 1, color.a or 0.6)
+    connection:Show()
+end
+
+function RoutePinMixin:OnLoad()
+    -- This is below normal handynotes pins
+    self:UseFrameLevelType(ns.CLASSIC and "PIN_FRAME_LEVEL_MAP_LINK" or "PIN_FRAME_LEVEL_EVENT_OVERLAY")
+    self:SetSize(12, 12)
+    self:EnableMouse()
+    self:SetMouseMotionEnabled(true)
+    self:SetMouseClickEnabled(true)
+
+    self.texture = self:CreateTexture(nil, "ARTWORK")
+    self.texture:SetAtlas("PlayerPartyBlip")
+    self.texture:SetAllPoints()
+end
+
+function RoutePinMixin:OnAcquired(variant, color)
+    self.variant = variant
+    self.texture:SetVertexColor(color.r, color.g, color.b, color.a or 1)
+end
+
+function RoutePinMixin:OnMouseEnter()
+    if self:GetCenter() > UIParent:GetCenter() then -- compare X coordinate
+        GameTooltip:SetOwner(self, "ANCHOR_LEFT")
+    else
+        GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+    end
+    local color = COLORS[self.variant]
+    GameTooltip:AddDoubleLine("Route", self.variant, 1, 1, 1, color.r, color.g, color.b)
+    GameTooltip:AddDoubleLine(" ", ns:GetCoord(self.normalizedX, self.normalizedY))
+    GameTooltip:Show()
+end
+
+function RoutePinMixin:OnMouseLeave()
+    GameTooltip:Hide()
+end
+
+function RoutePinMixin:OnMouseUp(button)
+    if button == "RightButton" then
+        ns:ShowConfigMenu()
+    end
+end
+
+function RoutePinConnectionMixin:Connect(pin1, pin2)
+    self:SetParent(pin1)
+    -- Anchor straight up from the origin
+    self:SetPoint("BOTTOM", pin1, "CENTER")
+
+    if not (pin1:GetCenter() and pin2:GetCenter()) then
+        -- I'm seeing reports of errors in CalculateAngleBetween which would imply one of the pins
+        -- isn't returning a center. I can't reproduce this to test it, but I think aborting here
+        -- should avoid errors.
+        return
+    end
+
+    -- Then adjust the height to be the length from origin to pin
+    local length = RegionUtil.CalculateDistanceBetween(pin1, pin2) * pin1:GetEffectiveScale()
+    self:SetHeight(length)
+    -- And finally rotate all the textures around the origin so they line up
+    local quarter = (math.pi / 2)
+    local angle = RegionUtil.CalculateAngleBetween(pin1, pin2) - quarter
+    self:RotateTextures(angle, 0.5, 0)
+
+    if ns.CLASSIC then
+        -- self.Line:SetTexture("Interface\\TaxiFrame\\UI-Taxi-Line")
+        self.Line:SetAtlas("_UI-Taxi-Line-horizontal")
+    else
+        self.Line:SetAtlas("_AnimaChannel-Channel-Line-horizontal")
+    end
+
+    self.Line:SetStartPoint("CENTER", pin1)
+    self.Line:SetEndPoint("CENTER", pin2)
+
+    self.Line:SetThickness(20)
+end
+
+do
+    -- This is MapCanvasMixin.AcquirePin lightly rewritten to not require an
+    -- XML template, so this can be bundled in with addons without requiring
+    -- a custom bit of XML for each one.
+    local function OnPinMouseUp(pin, button, upInside)
+        pin:OnMouseUp(button)
+        if upInside then
+            pin:OnClick(button)
+        end
+    end
+    function RouteWorldMapDataProvider:AcquirePin(...)
+        local pin, newPin = self.pinPool:Acquire()
+        pin.owningMap = self:GetMap()
+        if newPin then
+            pin:OnLoad()
+            local isMouseClickEnabled = pin:IsMouseClickEnabled()
+            local isMouseMotionEnabled = pin:IsMouseMotionEnabled()
+
+            if isMouseClickEnabled then
+                pin:SetScript("OnMouseUp", OnPinMouseUp)
+                pin:SetScript("OnMouseDown", pin.OnMouseDown)
+            end
+
+            if isMouseMotionEnabled then
+                pin:SetScript("OnEnter", pin.OnMouseEnter)
+                pin:SetScript("OnLeave", pin.OnMouseLeave)
+            end
+
+            pin:SetMouseClickEnabled(isMouseClickEnabled)
+            pin:SetMouseMotionEnabled(isMouseMotionEnabled)
+        end
+
+        self:GetMap().ScrollContainer:MarkCanvasDirty()
+        pin:Show()
+        pin:OnAcquired(...)
+
+        return pin
+    end
+end
+
